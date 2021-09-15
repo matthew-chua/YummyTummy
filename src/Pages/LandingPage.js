a7742b8080b80a318544d1fc7d542b86aac73b6b
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2021-09-15 15:21:18 +0800

    google sign in button

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,10 +2,10 @@ import { useEffect, useContext } from "react";
 import { useHistory } from "react-router";
 import { signInWithGoogleAuth, provider, auth } from "../Auth/auth";
 import { AuthContext } from "../Auth/AuthProvider";
-import pageClasses from "./Page.module.css"
-import landingPageClasses from "./LandingPage.module.css"
+import pageClasses from "./Page.module.css";
+import landingPageClasses from "./LandingPage.module.css";
 
-import logo from "../Assets/YummyTummyLogo.svg"
+import logo from "../Assets/YummyTummyLogo.svg";
 import hungryPic from "../Assets/food_dream.svg";
 
 export default function WelcomePage() {
@@ -26,24 +26,39 @@ export default function WelcomePage() {
 
   return (
     <div className={landingPageClasses.page}>
-
       <div>
-        <img src={logo} className={landingPageClasses.logo}/>
+        <img src={logo} className={landingPageClasses.logo} />
         <h4 className={landingPageClasses.h4}>not sure where to eat?</h4>
-        <p>fret not, Yummy Tummy is here to help you 
-          and your best friends find new places to dine!</p>
+        <p>
+          fret not, Yummy Tummy is here to help you and your best friends find
+          new places to dine!
+        </p>
 
         <div>
-          <button onClick={signInWithGoogleHandler}>Login</button>
+          <div>
+            <button
+              onClick={signInWithGoogleHandler}
+              className={landingPageClasses.btn}
+            >
+              <div className={landingPageClasses.btnContainer}>
+                <img
+                  className={landingPageClasses.img}
+                  src="https://developers.google.com/identity/images/g-logo.png"
+                />
+                <div className={landingPageClasses.btnText}>
+                  Sign in With Google
+                </div>
+              </div>
+            </button>
+          </div>
+
           <p>log in now to jio your friends!</p>
         </div>
-
       </div>
 
       <div>
-        <img src={hungryPic} className={landingPageClasses.pic}/>
+        <img src={hungryPic} className={landingPageClasses.pic} />
       </div>
-
     </div>
   );
 }
