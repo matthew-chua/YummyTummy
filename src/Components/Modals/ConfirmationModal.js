502db6fcca14bd05f451d9def3990712815e1449
matthew <mattchua2603@gmail.com>
2021-09-16 10:50:56 +0800

    updated docs

---
+++ src/Components/Modals/ConfirmationModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from "./ConfirmationModal.module.css";
+
+export default function ConfirmationModal() {
+    return (
+        <div className={classes.root}>
+            <h1>THis is the confirmation modal</h1>
+        </div>
+    )
+}
---
+++ src/Firestore/DatabaseManager.js
@@ -0,0 +1,3 @@
+import { collection, getDocs } from "firebase/firestore";
+import {app, db} from '../Firebase/firebase';
+
