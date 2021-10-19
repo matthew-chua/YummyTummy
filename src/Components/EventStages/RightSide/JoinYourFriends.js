473a290dfb870c4153c370835b977602bb6bfed6
Colin Hong <colin.hong18@gmail.com>
2021-10-19 18:14:03 +0800

    Dynamic host name for Join Your Friends

--- src/Components/EventStages/RightSide/JoinYourFriends.js
+++ src/Components/EventStages/RightSide/JoinYourFriends.js
@@ -116,13 +116,15 @@ export default function JoinYourFriends(props) {
       <div>
         <h1 className={classes.join}>Join Your Friends</h1>
         <h3 className={classes.text1}>
-          Respond to Matthew’s invite with your location!
+          Respond to {updatedParticipantsID[0].name}’s invite with your
+          location!
         </h3>
         {/* NOTE: STILL NEEDA PASS IN PROPS AND MAKE NAME RESPONSIVE */}
         <h3 className={classes.text2}>
           We’ll take your location into consideration, before suggesting a
-          handful of optimal diners for Matthew to choose based on their
-          location, reviews and what’s still open at this time!
+          handful of optimal diners for {updatedParticipantsID[0].name} to
+          choose based on their location, reviews and what’s still open at this
+          time!
         </h3>
       </div>
       <div className={classes.bottom}>
